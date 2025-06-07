9496c54e60ada0ab8d17c55b58cb0c54eeffdad3
prajwalsakore <112922796+prajwalsakore@users.noreply.github.com>
2025-06-07 22:08:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,3 +8,17 @@ def store_user_info(username, email, role):
         df.to_csv("user_data.csv", mode='a', header=False, index=False)
     else:
         df.to_csv("user_data.csv", index=False)
+
+import json
+
+def store_user_info(username, email, role):
+    user_info = {
+        "username": username,
+        "email": email,
+        "role": role,
+        "plan": "Free"  # default, can update later from Plans & Billing page
+    }
+
+    with open("user_info.json", "w") as f:
+        json.dump(user_info, f)
+
